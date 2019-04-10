31b20f555758a65ff492f52fedcdbfaf7e524fb4
Pownthep <pownthepofficial@gmail.com>
2019-04-10 21:02:03 +0700

    added logo to landing page

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyA1IyHPGgIYN63cREuDxuwgYoMqyImtF8E",
+    authDomain: "course-chapion.firebaseapp.com",
+    databaseURL: "https://course-chapion.firebaseio.com",
+    projectId: "course-chapion",
+    storageBucket: "course-chapion.appspot.com",
+    messagingSenderId: "639700191623"
+  }
 };
